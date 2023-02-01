9828e1c41cd3eec15b79b1a16b56954c1e9bcd4b
Edmilson de Assis Abreu <ein.xmi@gmail.com>
2023-02-01 19:20:29 -0300

    chore: generate fiori app project

---
+++ app/users/webapp/Component.js
@@ -0,0 +1,35 @@
+/**
+ * eslint-disable @sap/ui5-jsdocs/no-jsdoc
+ */
+
+sap.ui.define([
+        "sap/ui/core/UIComponent",
+        "sap/ui/Device",
+        "ein/codes/maintain/users/model/models"
+    ],
+    function (UIComponent, Device, models) {
+        "use strict";
+
+        return UIComponent.extend("ein.codes.maintain.users.Component", {
+            metadata: {
+                manifest: "json"
+            },
+
+            /**
+             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+             * @public
+             * @override
+             */
+            init: function () {
+                // call the base component's init function
+                UIComponent.prototype.init.apply(this, arguments);
+
+                // enable routing
+                this.getRouter().initialize();
+
+                // set the device model
+                this.setModel(models.createDeviceModel(), "device");
+            }
+        });
+    }
+);
